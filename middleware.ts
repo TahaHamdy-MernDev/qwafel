e128cb9be303562cbcf05702399e5c9ce58fde60
TahaHamdy-MernDev <TahaHamdyMernDev@gmail.com>
2024-12-10 13:39:58 +0200

    fix middleware issue try-3

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,6 @@ import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
 export default createMiddleware(routing);
-
-// export const config = {
-//   matcher: ["/(ar|en|)/:path*"],
-// };
-
 export const config = {
-  matcher: ["/", "/(ar|en)?/:path*"]
+  matcher: ["/", "/(ar|en)/:path*"],
 };
